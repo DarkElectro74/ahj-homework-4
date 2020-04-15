15c5c98da66bbf2e7ba7ece6e7a6636606936d32
DarkElectro <electrodark74@gmail.com>
2020-04-15 12:47:43 +0500

    ?

--- src/js/app.js
+++
@@ -1,5 +0,0 @@
-import '../licenses.txt';
-import ValidateCardWidget from './ValidateCardWidget.js';
-
-const validateCardForm = new ValidateCardWidget(document.body);
-validateCardForm.bindToDOM();
